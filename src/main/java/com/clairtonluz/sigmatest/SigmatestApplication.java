90c5727adadf039c53e9c614c46840b22ea16d32
Clairton Luz <clairton.c.l@gmail.com>
2023-01-11 18:13:06 -0300

    init project

---
+++ src/main/java/com/clairtonluz/sigmatest/SigmatestApplication.java
@@ -0,0 +1,13 @@
+package com.clairtonluz.sigmatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigmatestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigmatestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clairtonluz/sigmatest/SigmatestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clairtonluz.sigmatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigmatestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
